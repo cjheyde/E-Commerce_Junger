8f3ee91d8de979dcd06be6d896961775179e6197
cjheyde <carlaheyde@gmail.com>
2025-03-30 01:08:10 +0100

    edit Navbar and css

---
+++ frontend/src/Components/Assets/Perfumes/all_perfumes.js
@@ -0,0 +1,21 @@
+import amarelinas from './amarelinhas_aroma.jpg'
+
+
+let all_perfumes = [
+    {
+        id: 1,
+        name: "xxx",
+        cathegory: "sweet",
+        image: amarelinas,
+        new_price: 40.0,
+        old_price: 20.0,
+    },
+    {
+        id: 2,
+        name: "xxx",
+        cathegory: "sweet",
+        image: p87_img,
+        new_price: 40.0,
+        old_price: 20.0,
+    },
+];
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import './Navbar.css'
+import logo from '../Assets/Perfumes/logo.png'
+import cart_icon from '../Assets/shopping-cart.png'
 
 const Navbar = () => {
-    return (
-        <div>
 
+    const [menu,setMenu] = useState("Perfumes");
+
+    return (
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="Perfume" width="50px" />
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() =>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={() =>{setMenu("vehicle")}}>Vehicles{menu==="vehicle"?<hr/>:<></>}</li>
+                <li onClick={() =>{setMenu("perfume")}}>Perfumes{menu==="perfume"?<hr/>:<></>}</li>
+            </ul>
+            <div className="nav-login_cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" width="40px" />
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
     )
 }
