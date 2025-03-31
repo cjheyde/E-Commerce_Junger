a305b5ec1f6b3645d5e0ebc08a31b235dae01685
cjheyde <carlaheyde@gmail.com>
2025-04-01 00:20:49 +0200

    add shop and change carro to cars

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react" 
 import './Navbar.css'
 import logo from '../Assets/logo.png'
-import cart from '../Assets/cart.png'
+import cart from '../Assets/shopping-cart2.png'
 
 
 const Navbar = () => {
 
-    const[menu, setMenu] = useState('Carro')
+    const[menu, setMenu] = useState('Cars')
 
     return (
         <div className="navbar">
             <div className="navlogo">
-                <img src={logo} alt="" width="50px"/>
+                <img src={logo} alt="logo" width="50px"/>
                 <p>SHOPPER</p>
             </div>
-            <ul className="nav-menu" > 
-                <li onClick={()=>{setMenu('Carro')}}>Carro {menu==='Carro'?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu('Perfumes')}}>Perfumes {menu==='Perfumes'?<hr/>:<></>}</li>
+            <ul className="nav-menu" >
+                <li onClick={() =>{setMenu("Shop")}}>Shop {menu==='Shop'?<hr/>:<></>}</li> 
+                <li onClick={() =>{setMenu('Cars')}}>Cars {menu==='Cars'?<hr/>:<></>}</li>
+                <li onClick={() =>{setMenu('Perfumes')}}>Perfumes {menu==='Perfumes'?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
-                <img src={cart} alt="" width="40px"/>
+                <img src={cart} alt="cart" width="40px"/>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>
