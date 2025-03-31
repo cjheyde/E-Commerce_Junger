b302afbdc2d1319a96bc3b538be71d81e138bcbe
cjheyde <carlaheyde@gmail.com>
2025-03-31 02:14:38 +0200

    navbar update

--- frontend/src/Components/Assets/Perfumes/all_perfumes.js
+++ frontend/src/Components/Assets/Perfumes/all_perfumes.js
@@ -1,20 +1,22 @@
 import amarelinas from './amarelinhas_aroma.jpg'
-
+import amor from './amor_aroma.jpg'
 
 let all_perfumes = [
     {
         id: 1,
-        name: "xxx",
-        cathegory: "sweet",
+        name: "Pequenas flores amarelas da primavera",
+        cathegory: "flower",
+        note: "fresh",
         image: amarelinas,
         new_price: 40.0,
         old_price: 20.0,
     },
     {
         id: 2,
-        name: "xxx",
-        cathegory: "sweet",
-        image: p87_img,
+        name: "Tres flores vermelhas do amor",
+        cathegory: "flower",
+        note: "sweet",
+        image: amor,
         new_price: 40.0,
         old_price: 20.0,
     },
